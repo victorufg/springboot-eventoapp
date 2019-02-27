54de0cbad35f6c9194d515854ac7426fcbca10e7
Victor Hugo <victorufgcc@hotmail.com>
2019-02-27 13:57:36 -0300

    primeiro commit

---
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -0,0 +1,73 @@
+package com.eventoapp.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.util.List;
+
+@SuppressWarnings("deprecation")
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long codigo;
+
+	@NotNull
+	@Size(min=3, max=15)
+	private String nome;
+	
+	@NotEmpty
+	private String local;
+	
+	@NotEmpty
+	private String data;
+
+	@NotEmpty
+	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidados;
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+}
